5bf5de7b99cd4839bc076b888f5af62c30e56b69
ardyn <ada.adiles@gmail.com>
2023-12-09 22:51:39 +0700

    edit db

--- config/db.js
+++ config/db.js
@@ -1,8 +1,16 @@
-const Sequelize = require('sequelize')
+const mysql = require('mysql')
+require('dotenv').config()
 
-const db = new Sequelize('user_db','root','',{
-    host: 'localhost',
-    dialect: 'mysql'
+const db = mysql.createConnection({
+    host: 'public_ip_sql_instance_Anda',
+    user: 'root',
+    database: 'nama_database_Anda',
+    password: 'password_sql_Anda'
+})
+
+db.connect((err) => {
+    if (err) throw err;
+    console.log('Connected to the database');
 });
 
 module.exports = db;
