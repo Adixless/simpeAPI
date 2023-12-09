4770d60169e9cb5f5edc2cb2e05100f5efe948aa
ardyn <ada.adiles@gmail.com>
2023-12-09 22:14:46 +0700

    rubah route

--- routes/route.js
+++ routes/route.js
@@ -18,13 +18,9 @@ router.post('/register', register);
 router.post('/login', login);
 router.get('/token', refreshToken);
 router.delete('/logout', logout);
-<<<<<<< HEAD
-router.get('/profile:id', getProfile);
-router.put('/editProfile:id', editProfile);
-=======
 router.get('/profile', getProfile);
 router.put('/editProfile', editProfile);
 router.post('/upload', cvUpload);
->>>>>>> a7c522cd84b7a4cf19366ff37f9beb4a02ce79db
+
 
 module.exports = router;
