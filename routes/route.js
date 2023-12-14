118fecc64b480ccffd541c73b86dcb696fec1e09
KadekArie <119066190+KadekArie@users.noreply.github.com>
2023-12-14 16:48:54 +0800

    update routes get job, get detailed job, apply job

--- routes/route.js
+++ routes/route.js
@@ -6,7 +6,10 @@ const {
     logout,
     getProfile,
     editProfile,
-    // cvUpload
+    // cvUpload,
+    getAllJob,
+    getDetailedJob,
+    jobApply
 } = require("../Controller/user.js");
 const verifyToken = require ("../middleware/verifyToken.js")
 const refreshToken = require ("../Controller/RefreshToken.js")
@@ -21,6 +24,9 @@ router.delete('/logout', logout);
 router.get('/users/:id', getProfile);
 router.put('/users/:id', editProfile);
 // router.post('/upload', cvUpload);
+router.get('/jobs', getAllJob);
+router.get('/job/:jobId', getDetailedJob);
+router.post('/apply/:jobId', jobApply);
 
 
 module.exports = router;
